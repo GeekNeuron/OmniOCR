df1d3dab04fb517ff6af24ee177fed45ae77180d
Geek Neuron <geekneuron@gmail.com>
2025-05-14 08:36:22 +0330

    build.gradle.kts

--- mobile/ocr-core/build.gradle.kts
+++ mobile/ocr-core/build.gradle.kts
@@ -1,1 +1,51 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.library")
+}
 
+kotlin {
+    android()
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+            }
+        }
+        val androidMain by getting {
+            dependencies {
+                implementation("androidx.annotation:annotation:1.6.0")
+            }
+        }
+        val iosMain by creating {
+            dependencies {
+                // Swift interop or cocoapods setup here
+            }
+        }
+    }
+}
+
+android {
+    namespace = "org.omniocr.ocrcore"
+    compileSdk = 33
+    defaultConfig {
+        minSdk = 26
+        targetSdk = 33
+        externalNativeBuild {
+            cmake {
+                cppFlags += "-std=c++17"
+            }
+        }
+        ndk {
+            abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86_64")
+        }
+    }
+    externalNativeBuild {
+        cmake {
+            path = file("src/androidMain/cpp/CMakeLists.txt")
+        }
+    }
+}
