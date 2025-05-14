cfe549c8ca09d045a0e50258cfa4cb64e2c2eb89
Geek Neuron <geekneuron@gmail.com>
2025-05-14 08:37:47 +0330

    OcrEngine_Android.kt

--- mobile/ocr-core/OcrEngine_Android.kt
+++ mobile/ocr-core/OcrEngine_Android.kt
@@ -1,1 +1,13 @@
+package org.omniocr
 
+actual class OcrEngine {
+    init {
+        System.loadLibrary("OCRBridge")
+    }
+
+    external fun runOcrNative(imagePath: String, langCode: String): String
+
+    actual fun runOcr(imagePath: String, langCode: String): String {
+        return runOcrNative(imagePath, langCode)
+    }
+}
