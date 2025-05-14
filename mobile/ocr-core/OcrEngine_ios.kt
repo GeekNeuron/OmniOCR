3e3d972ff8aac194f4f3944181cfd33562e49eea
Geek Neuron <geekneuron@gmail.com>
2025-05-14 08:38:31 +0330

    OcrEngine_ios.kt

--- mobile/ocr-core/OcrEngine_ios.kt
+++ mobile/ocr-core/OcrEngine_ios.kt
@@ -1,1 +1,14 @@
+package org.omniocr
 
+import platform.Foundation.*
+import kotlinx.cinterop.*
+import platform.darwin.*
+
+@CName("runOcrIOS")
+external fun runOcrIOS(imagePath: String, langCode: String): String
+
+actual class OcrEngine {
+    actual fun runOcr(imagePath: String, langCode: String): String {
+        return runOcrIOS(imagePath, langCode)
+    }
+}
