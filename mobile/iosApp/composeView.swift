4149288b29fa368d645c78a013d7d67ffc8a4e92
Geek Neuron <geekneuron@gmail.com>
2025-05-14 10:40:14 +0330

    composeView.swift

--- mobile/iosApp/composeView.swift
+++ mobile/iosApp/composeView.swift
@@ -1,1 +1,19 @@
+import SwiftUI
+import shared
+import UIKit
 
+struct ComposeView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        Main_iosKt.MainScreenViewController(
+            engine: OcrEngine(),
+            onShare: { text in
+                let vc = UIActivityViewController(activityItems: [text], applicationActivities: nil)
+                if let topVC = UIApplication.shared.windows.first?.rootViewController {
+                    topVC.present(vc, animated: true)
+                }
+            }
+        )
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+}
